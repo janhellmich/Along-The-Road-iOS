c8211f7c7a3cd8ceff404a88ed456e0725b9df47
Linus Meyer-Teruel <lmeyerte@stanford.edu>
2015-08-29 10:16:43 -0700

    Initial Commit

---
+++ AlongTheRoad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlongTheRoad
+//
+//  Created by Linus Aidan Meyer-Teruel on 8/29/15.
+//  Copyright (c) 2015 Linus Aidan Meyer-Teruel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
