1f25eeeac4cbf47caf7e83aa5bd662febbe53b79
Linus Meyer-Teruel <lmeyerte@stanford.edu>
2015-09-02 10:57:51 -0700

    (feat) Added the apple map and made it start on current location

--- AlongTheRoad/MapViewController.swift
+++ AlongTheRoad/MapViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     var destination: String?
     var startingPoint: String?
     
+    var coreLocationManager = CLLocationManager()
+    
+    var locationManager:LocationManager!
     
     @IBOutlet weak var destLabel: UILabel!
     @IBOutlet weak var startLabel: UILabel!
@@ -22,26 +26,48 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.startLabel.text = "Destination: " +  self.destination!
-        self.destLabel.text = "Start: " + self.startingPoint!
+        coreLocationManager.delegate = self
+        
+        locationManager = LocationManager.sharedInstance
+        
+        let authorizationCode = CLLocationManager.authorizationStatus()
+        if authorizationCode == CLAuthorizationStatus.NotDetermined && coreLocationManager.respondsToSelector("requestAlwaysAuthorization") || coreLocationManager.respondsToSelector("requestWhenInUseAuthorization") {
+            if NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") != nil {
+                coreLocationManager.requestAlwaysAuthorization()
+            } else {
+                getLocation()
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.NotDetermined || status != CLAuthorizationStatus.Denied || status != CLAuthorizationStatus.Restricted {
+            getLocation()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getLocation(){
+        locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
+            self.displayLocation(CLLocation(latitude: latitude, longitude: longitude))
+        }
     }
-    */
+    
+    func displayLocation(location:CLLocation){
+        map.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude), span: MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        let locationPinCoord = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationPinCoord
+        map.addAnnotation(annotation)
+        map.showAnnotations([annotation], animated: true)
+        
+        locationManager.reverseGeocodeLocationUsingGoogleWithCoordinates(location, onReverseGeocodingCompletionHandler: { (reverseGecodeInfo, placemark, error) -> Void in
+            let address = reverseGecodeInfo?.objectForKey("formattedAddress") as! String
+//            self.myLocation.text = address
+            print(address)
+        })
+
+    }
 
 }
